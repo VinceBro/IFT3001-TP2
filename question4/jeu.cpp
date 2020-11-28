13ac44d34c2807661b6eb31b7cf1e8625feac388
Vincent Breault <vincent.breault.1@ulaval.ca>
2020-11-28 16:23:34 -0500

    100% question 4

--- question4/jeu.cpp
+++ question4/jeu.cpp
@@ -14,21 +14,22 @@ std::vector<Monstre> programmationDynamique(const std::vector<Monstre>& monstres
   Tableau<unsigned int> tab(dimensions);
 
   for (int x = 1; x<= n; x++) {
-    for (int y = 1; y<= magie_rouge; y++ ) {
-      for (int z = 1; z <= magie_bleue; z++) {
+    for (int y = 0; y <= magie_rouge; y++ ) {
+      for (int z = 0; z <= magie_bleue; z++) {
 
+        int chose2, chose1;
+        int valeur = tab.at(x-1, y, z);
         int coeffRouge = y - monstres_c[x].magieRouge();
+        if(coeffRouge >= 0 ) {
+          chose1 = monstres_c[x].degat() + tab.at(x - 1, coeffRouge, z);
+        }
         int coeffBleu = z - monstres_c[x].magieBleue();
+        if (coeffBleu >=0 ){
+          chose2 = monstres_c[x].degat() + tab.at(x - 1, y ,coeffBleu);
+        }
         if (coeffRouge >= 0 && coeffBleu >= 0) {
-          tab.at(x, y, z) = std::max({tab.at(x - 1, y, z), monstres_c[x].degat() + tab.at(x - 1, coeffRouge, z), monstres_c[x].degat() + tab.at(x - 1, y, coeffBleu), monstres_c[x].degat() + tab.at(x-1, coeffRouge, coeffBleu)});
+          tab.at(x, y, z) = std::max({tab.at(x - 1, y, z), monstres_c[x].degat() + tab.at(x - 1, coeffRouge, coeffBleu)});
         }
-/*
-        else if (coeffRouge >= 0) {
-          tab.at(x, y, z) = std::max(tab.at( x - 1, y, z), monstres_c[x].degat() + tab.at(x - 1, coeffRouge, z));
-        } else if (coeffBleu >= 0) {
-          tab.at(x, y, z) = std::max(tab.at( x - 1, y, z), monstres_c[x].degat() + tab.at(x - 1, y, coeffBleu));
-        }
-*/
         else {
           tab.at(x, y, z) = tab.at(x-1, y, z);
         }
@@ -41,14 +42,15 @@ std::vector<Monstre> programmationDynamique(const std::vector<Monstre>& monstres
   for (int i = n; i >= 1; i--) {
     int sauce1 = tab.at(i, magie_rouge_c, magie_bleue_c);
     int sauce2 = tab.at(i-1, magie_rouge_c, magie_bleue_c);
+    Monstre monstre = monstres_c[i];
     int magie_rouge_monstre = monstres_c[i].magieRouge();
     int magie_bleue_monstre = monstres_c[i].magieBleue();
 
-    if (tab.at(i, magie_rouge_c, magie_bleue_c) > tab.at(i-1, magie_rouge_c, magie_bleue_c) && monstres_c[i].magieRouge() <= magie_rouge_c && monstres_c[i].magieBleue() <= magie_bleue_c){
-      L.push_back(monstres_c[i]);
-      magie_rouge_c -= monstres_c[i].magieRouge();
+    if (tab.at(i, magie_rouge_c, magie_bleue_c) > tab.at(i-1, magie_rouge_c, magie_bleue_c) && monstre.magieRouge() <= magie_rouge_c && monstre.magieBleue() <= magie_bleue_c){
+      L.push_back(monstre);
+      magie_rouge_c -= monstre.magieRouge();
       if (magie_rouge_c < 0) throw std::exception();
-      magie_bleue_c -= monstres_c[i].magieBleue();
+      magie_bleue_c -= monstre.magieBleue();
       if (magie_bleue_c < 0) throw std::exception();
     }
 
